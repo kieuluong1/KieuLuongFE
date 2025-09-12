3ed4e5c693f0b699da91039229b328573b1e3eb1
IzooBeeee <vannhan130504@gmail.com>
2025-09-12 16:15:57 +0700

    first commit

---
+++ src/layout/components/BotClient.vue
@@ -0,0 +1,106 @@
+<template>
+    <footer class="text-center text-lg-start text-white" style="background-color: #1c2331">
+        <!-- Section: Social media -->
+        <section class="d-flex justify-content-between p-4" style="background-color: #6351ce">
+            <!-- Left -->
+            <div class="me-5">
+            </div>
+            <!-- Left -->
+
+            <!-- Right -->
+            <div>
+                <a href="" class="text-white me-4">
+                    <i class="fab fa-facebook-f"></i>
+                </a>
+                <a href="" class="text-white me-4">
+                    <i class="fab fa-twitter"></i>
+                </a>
+                <a href="" class="text-white me-4">
+                    <i class="fab fa-google"></i>
+                </a>
+                <a href="" class="text-white me-4">
+                    <i class="fab fa-instagram"></i>
+                </a>
+                <a href="" class="text-white me-4">
+                    <i class="fab fa-linkedin"></i>
+                </a>
+                <a href="" class="text-white me-4">
+                    <i class="fab fa-github"></i>
+                </a>
+            </div>
+            <!-- Right -->
+        </section>
+
+        <section class="">
+            <div class="container text-center text-md-start mt-5">
+                <div class="row mt-3">
+                    <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+                        <h6 class="text-uppercase fw-bold">
+                            VAN NHAN</h6>
+                        <hr class=" mt-0 d-inline-block mx-auto"
+                            style="width: 60px; background-color: #7c4dff; height: 2px" /><br>
+                        <div class="text-center mb-2">
+                        </div>
+                        <p>
+                            Here you can use rows and columns to organize your footer
+                            content. Lorem ipsum dolor sit amet, consectetur adipisicing
+                            elit.
+                        </p>
+                    </div>
+                    <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
+                        <h6 class="text-uppercase fw-bold">Products</h6>
+                        <hr class="mb-4 mt-0 d-inline-block mx-auto"
+                            style="width: 60px; background-color: #7c4dff; height: 2px" />
+                        <p>
+                            <a href="#!" class="text-white">Home</a>
+                        </p>
+                        <p>
+                            <a href="#!" class="text-white">About</a>
+                        </p>
+                        <p>
+                            <a href="#!" class="text-white">Product</a>
+                        </p>
+                        <p>
+                            <a href="#!" class="text-white">Contact</a>
+                        </p>
+                    </div>
+                    <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+                        <h6 class="text-uppercase fw-bold">Useful links</h6>
+                        <hr class="mb-4 mt-0 d-inline-block mx-auto"
+                            style="width: 60px; background-color: #7c4dff; height: 2px" />
+                        <p>
+                            <a href="#!" class="text-white">Facebook</a>
+                        </p>
+                        <p>
+                            <a href="#!" class="text-white">Instagram</a>
+                        </p>
+                        <p>
+                            <a href="#!" class="text-white">Tripavisor</a>
+                        </p>
+                        <p>
+                            <a href="#!" class="text-white">Twitter</a>
+                        </p>
+                    </div>
+                    <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+                        <h6 class="text-uppercase fw-bold">Contact</h6>
+                        <hr class="mb-4 mt-0 d-inline-block mx-auto"
+                            style="width: 60px; background-color: #7c4dff; height: 2px" />
+                        <p><i class="fas fa-home mr-3"></i>61 Lý Tự Trọng, Hải Châu, Đà Nẵng</p>
+                        <p><i class="fas fa-envelope mr-3"></i> vannhan130504@gmail.com</p>
+                        <p><i class="fas fa-phone mr-3"></i> + 0987654321</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2)">
+            © 2024 Copyright:
+            <a class="text-white" href="/">VAN NHAN</a>
+        </div>
+    </footer>
+</template>
+<script>
+export default {
+
+}
+</script>
+<style></style>
